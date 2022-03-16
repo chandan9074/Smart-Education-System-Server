2ed34ff22c27dce1561ecac2aac20c11ee0dc107
chandan9074 <chandan15-11611@diu.edu.bd>
2022-03-16 08:28:14 +0600

    feat: create courses, classes, student, teacher model

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from courses.models import Classes, Courses
 # Register your models here.
+
+admin.site.register(Classes)
+admin.site.register(Courses)
--- courses/models.py
+++ courses/models.py
@@ -1,11 +1,26 @@
+from pyexpat import model
 from django.db import models
 
+from accounts.models import StudentPorfile, TeacherPorfile
 # Create your models here.
+
+class Classes(models.Model):
+    class_name = models.CharField(max_length=20)
+    section = models.CharField(max_length=20)
+    students = models.ManyToManyField(StudentPorfile)
+
 
 class Courses(models.Model):
 
     title = models.CharField(max_length=200)
     description = models.TextField()
     course_code = models.CharField(max_length=100)
-    class_name = models.CharField(max_length=20)
+    classes = models.ManyToManyField(Classes)
+    instructor = models.ForeignKey(TeacherPorfile, on_delete=models.CASCADE, null=True)
+
+# class CourseContents(models.Model):
+#     title = models.CharField(max_length=500)
+#     details = models.TextField()
+
+    
     
